35dc94253152bf64777c55cb909a27e20d2e3b5b
dragonhunt02 <jackhunt43@libero.it>
2025-07-17 21:24:14 +0200

    Update enet_sup.erl

--- src/enet_sup.erl
+++ src/enet_sup.erl
@@ -31,6 +31,30 @@ start_host_supervisor(Port, ConnectFun, Options) ->
     },
     supervisor:start_child(?MODULE, Child).
 
+start_host_dtls_supervisor(Port, ConnectFun, Options) ->
+    %%Port = 5684,
+    %% Only one host supported for now
+    %% Listener: the socket listener gen_server
+    Listener = #{
+        id => listener,
+        start => {dtls_echo_listener, start_link, [Port, ConnectFun, Options]},
+        restart => permanent,
+        shutdown => 5000, %%infinity,
+        type => worker,
+        modules => [dtls_echo_listener]
+    },
+    %% ConnSup: dynamic supervisor for connections
+    ConnSup = #{
+        id => connection_sup,
+        start => {dtls_echo_conn_sup, start_link, [Port, ConnectFun, Options]},
+        restart => permanent,
+        shutdown => 5000, %%infinity,
+        type => supervisor,
+        modules => [dtls_echo_conn_sup]
+    },
+    supervisor:start_child(?MODULE, Listener),
+    supervisor:start_child(?MODULE, ConnSup).
+
 stop_host_supervisor(HostSup) ->
     supervisor:terminate_child(?MODULE, HostSup).
 
@@ -41,7 +65,7 @@ stop_host_supervisor(HostSup) ->
 init([]) ->
     SupFlags = #{
         strategy => one_for_one,
-        intensity => 1,
+        intensity => 1, %% try 10,10
         period => 5
     },
     {ok, {SupFlags, []}}.
