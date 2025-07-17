48fcbcbcc6378654af13d174ca2d9fb4ced351ed
dragonhunt02 <jackhunt43@libero.it>
2025-07-17 03:27:23 +0200

    Create enet_host_dtls_sup.erl

---
+++ src/enet_host_dtls_sup.erl
@@ -0,0 +1,94 @@
+-module(enet_host_sup).
+-behaviour(supervisor).
+
+%% API
+-export([
+    start_link/3
+]).
+
+%% Supervisor callbacks
+-export([init/1]).
+
+%%%===================================================================
+%%% API functions
+%%%===================================================================
+
+start_link(Port, ConnectFun, Options) ->
+    supervisor:start_link(?MODULE, [Port, ConnectFun, Options]).
+
+%%%===================================================================
+%%% Supervisor callbacks
+%%%===================================================================
+
+init([Port, ConnectFun, Options]) ->
+    SupFlags = #{
+        strategy => one_for_one,
+        intensity => 1,
+        period => 5
+    },
+    PeerLimit =
+        case lists:keyfind(peer_limit, 1, Options) of
+            {peer_limit, PLimit} -> PLimit;
+            false -> 1
+        end,
+    Pool = #{
+        id => enet_pool,
+        start => {
+            enet_pool,
+            start_link,
+            [Port, PeerLimit]
+        },
+        restart => permanent,
+        shutdown => 2000,
+        type => worker,
+        modules => [enet_pool]
+    },
+    Host = #{
+        id => enet_host,
+        start => {
+            enet_host,
+            start_link,
+            [Port, ConnectFun, Options]
+        },
+        restart => permanent,
+        shutdown => 2000,
+        type => worker,
+        modules => [enet_host]
+    },
+    Disconnector = #{
+        id => enet_disconnector,
+        start => {
+            enet_disconnector,
+            start_link,
+            [Port]
+        },
+        restart => permanent,
+        shutdown => 1000,
+        type => worker,
+        modules => [enet_disconnector]
+    },
+    PeerSup = #{
+        id => enet_peer_sup,
+        start => {
+            enet_peer_sup,
+            start_link,
+            [Port]
+        },
+        restart => permanent,
+        shutdown => infinity,
+        type => supervisor,
+        modules => [enet_peer_sup]
+    },
+    Acceptor = #{
+      id        => dtls_acceptor,
+      start     => {enet_dtls_acceptor, start_link, [Port, ConnectFun, Options]},
+      restart   => permanent,
+      shutdown  => 5000,
+      type      => worker,
+      modules   => [enet_dtls_acceptor]
+    },
+    {ok, {SupFlags, [Acceptor, Pool, Host, Disconnector, PeerSup]}}.
+
+%%%===================================================================
+%%% Internal functions
+%%%===================================================================
