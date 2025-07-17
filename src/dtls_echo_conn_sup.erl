c651f8f7fa7406512b8e8d7cb40e5fbe803ebe46
dragonhunt02 <jackhunt43@libero.it>
2025-07-17 21:27:18 +0200

    Create dtls_echo_conn_sup.erl

---
+++ src/dtls_echo_conn_sup.erl
@@ -0,0 +1,25 @@
+%% src/dtls_echo_conn_sup.erl
+-module(dtls_echo_conn_sup).
+-behaviour(supervisor).
+
+-export([start_link/0, init/1, start_child/2]).
+
+start_link() ->
+    supervisor:start_link({local, ?MODULE}, ?MODULE, []).
+
+init([]) ->
+    %% Each connection is a dtls_echo_server child
+    ConnChild = {
+      dtls_conn,
+      {dtls_echo_server, start_link, []},
+      transient,
+      5000,
+      worker,
+      [dtls_echo_server]
+    },
+
+    {ok, {{simple_one_for_one, 5, 10}, [ConnChild]}}.
+
+%% Called by listener when a new socket arrives
+start_child(Transport, Socket) ->
+    supervisor:start_child(?MODULE, [Transport, Socket]).
