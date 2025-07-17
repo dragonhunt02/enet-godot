1da859faaea3d48661d141b7a8caef7fc34a0ece
dragonhunt02 <jackhunt43@libero.it>
2025-07-17 21:28:52 +0200

    Create dtls_echo_listener.erl

---
+++ src/dtls_echo_listener.erl
@@ -0,0 +1,45 @@
+%% src/dtls_echo_listener.erl
+-module(dtls_echo_listener).
+-behaviour(gen_server).
+
+-export([start_link/1]).
+-export([init/1, handle_info/2, terminate/2, code_change/3]).
+
+-record(state, {
+  port
+}).
+
+%%% API
+start_link(Port) ->
+    gen_server:start_link({local, ?MODULE}, ?MODULE, Port, []).
+
+%%% gen_server callbacks
+init(Port) ->
+    ok = esockd:start(),
+    PrivDir = code:priv_dir(esockd),
+    DtlsOpts = [
+      {mode, binary}, {reuseaddr, true}, {active, 100},
+      {certfile, filename:join(PrivDir, "demo.crt")},
+      {keyfile,  filename:join(PrivDir, "demo.key")}
+    ],
+    Opts = [
+      {acceptors, 4},
+      {max_connections, 1000},
+      {dtls_options, DtlsOpts}
+    ],
+
+    %% Tell esockd to use our connection‐sup to spawn each handler
+    MFArgs = {dtls_echo_conn_sup, start_child, []},
+    {ok, _ListenSock} = esockd:open_dtls('echo/dtls', Port, Opts, MFArgs),
+
+    {ok, #state{port=Port}}.
+
+handle_info(_Info, State) ->
+    %% We don’t expect “normal” messages here
+    {noreply, State}.
+
+terminate(_Reason, _State) ->
+    ok.
+
+code_change(_OldVsn, State, _Extra) ->
+    {ok, State}.
