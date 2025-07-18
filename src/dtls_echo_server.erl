ff1af7abfef7a4088c2463aff4fbb47fd9a4efc2
dragonhunt02 <jackhunt43@libero.it>
2025-07-18 23:13:04 +0200

    Update dtls_echo_server.erl

--- src/dtls_echo_server.erl
+++ src/dtls_echo_server.erl
@@ -3,7 +3,8 @@
 -behaviour(gen_server).
 
 -export([start_link/2]).
--export([init/1, handle_continue/2, handle_info/2, handle_cast/2, handle_call/3, terminate/2, code_change/3]).
+-export([init/1, handle_info/2, handle_cast/2, handle_call/3, terminate/2, code_change/3]).
+%% handle_continue/2, 
 
 -record(state, {
   transport,
